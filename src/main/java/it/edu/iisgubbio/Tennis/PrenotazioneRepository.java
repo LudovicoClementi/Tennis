f776b955a5679a33434a6c49ff2a96823f35f1eb
Criii06 <fgn0823@gmail.com>
2025-04-28 11:55:58 +0200

    seconda richeista funzionante

--- src/main/java/it/edu/iisgubbio/Tennis/PrenotazioneRepository.java
+++ src/main/java/it/edu/iisgubbio/Tennis/PrenotazioneRepository.java
@@ -1,7 +1,21 @@
 package it.edu.iisgubbio.Tennis;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Integer>{
-
+	@Query(value="SELECT prenotazione.* "
+			+ "FROM prenotazione,campo "
+			+ "WHERE prenotazione.campo_id=campo.id "
+			+ "AND prenotazione.data=:dataPre "
+			+ "AND campo.id=:idCampo",
+			nativeQuery=true)
+			List<Prenotazione>cerca(
+					@Param("idCampo") Integer idCampo,
+					@Param("dataPre") String dataPre)
+					;
 }
+//SISTEMARE QUERY SOPRA
--- src/main/java/it/edu/iisgubbio/Tennis/TennisManager.java
+++ src/main/java/it/edu/iisgubbio/Tennis/TennisManager.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController 
 public class TennisManager {
 	@Autowired
@@ -19,16 +20,32 @@ public class TennisManager {
 	@Autowired 
 	PrenotazioneRepository archivioPrenotazione;
 	
+	//TUTTI I CAMPI
 	@GetMapping("/campi")
 	public List<Campo> mostraCampo(){
 		return archivioCampo.findAll();
 		}
 	
-
+	//TUTTE LE PRENOTAZIONI
 	@GetMapping("/prenotazione")
 	public List<Prenotazione> mostraPrenotazione(){
 		return archivioPrenotazione.findAll();
 		}
 	
+	//UN CAMPO
+	@GetMapping("/campo/{id}")
+	public Optional<Campo> mostraCampo(@PathVariable Integer id) {
+		
+		return archivioCampo.findById(id);
+	}
 	
+	//d
+	@GetMapping("/campo/{id}/prenotazione")
+	public List<Prenotazione> mostraPrenData(
+			@PathVariable Integer id,
+			@RequestParam String data
+			) {
+		
+		return archivioPrenotazione.cerca(id,data);
+	}
 }
